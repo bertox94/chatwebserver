6e8ea500480f795bfdf97f1753d95b448a9bc63d
Studente <halibert94@gmail.com>
2025-01-24 08:24:09 +0100

    Initial commit

---
+++ src/main/java/com/example/chatwebserver/ChatwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatwebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatwebserver/ChatwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
